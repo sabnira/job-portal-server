f2668162333d7f9250361d77ee36fcca5b5e2ae2
sabnira <sabnirajeemi@gmail.com>
2025-10-19 21:14:36 +0600

    get

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -37,6 +37,13 @@ async function run() {
         app.get('/jobs', async(req, res) => {
             const cursor = jobsCollection.find();
             const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await jobsCollection.findOne(query);
             res.send(result);
         })
 
